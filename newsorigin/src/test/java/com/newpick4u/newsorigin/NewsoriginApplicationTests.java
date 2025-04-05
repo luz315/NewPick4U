429b57dfe748766de150439104c50d57c1a213a4
jeonjinwoo <wls45al@gmail.com>
2025-04-05 20:30:30 +0900

    [#13][newsorign] feat: 원본뉴스 서비스 환경구성

---
+++ newsorigin/src/test/java/com/newpick4u/newsorigin/NewsoriginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newpick4u.newsorigin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsoriginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
