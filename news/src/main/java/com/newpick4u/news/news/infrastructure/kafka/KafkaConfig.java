c1ee813ded8e1fee0309f8340fb8a9e547a8d61e
poorm <315luz10@gmail.com>
2025-04-06 15:27:53 +0900

    [#4][news] refactor: 카프카 설정 값 로직 분리

--- news/src/main/java/com/newpick4u/news/news/infrastructure/kafka/KafkaConfig.java
+++ news/src/main/java/com/newpick4u/news/news/infrastructure/kafka/KafkaConfig.java
@@ -1,19 +1,50 @@
 package com.newpick4u.news.news.infrastructure.kafka;
 
+import com.newpick4u.news.news.infrastructure.kafka.dto.AiNewsDto;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.util.backoff.FixedBackOff;
 
-import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
+
+    @Bean
+    public ConsumerFactory<String, AiNewsDto> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "${KAFKA_BOOTSTRAP_SERVERS}");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "${KAFKA_GROUP_ID}");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+
+        JsonDeserializer<AiNewsDto> valueDeserializer = new JsonDeserializer<>(AiNewsDto.class);
+        valueDeserializer.addTrustedPackages("com.newpick4u.news.news.infrastructure.kafka.dto");
+
+        return new DefaultKafkaConsumerFactory<>(
+                props,
+                new StringDeserializer(),
+                valueDeserializer
+        );
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(
