4459518470c29f7ff602b15d029649fc36180ad3
leedongha1998 <leedongha98@gmail.com>
2025-04-08 01:49:49 +0900

    [#32][thread] feat : Thread 도메인 feignClient 관련 설정 추가 - timeout - 재시도 - fallback

--- thread/src/main/java/com/newpick4u/thread/thread/infrastructure/client/CommentClient.java
+++ thread/src/main/java/com/newpick4u/thread/thread/infrastructure/client/CommentClient.java
@@ -1,12 +1,14 @@
 package com.newpick4u.thread.thread.infrastructure.client;
 
+import com.newpick4u.thread.global.config.CommentClientFallback;
+import com.newpick4u.thread.global.config.FeignClientConfig;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "comment-service")
+@FeignClient(name = "comment-service", configuration = FeignClientConfig.class, fallback = CommentClientFallback.class)
 public interface CommentClient {
 
   @GetMapping("/api/internal/comments/count/threads/{threadId}")
