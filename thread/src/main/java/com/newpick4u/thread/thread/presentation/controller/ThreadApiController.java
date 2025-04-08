f55b9b74cc4e007512a7b2a310f2d1878402fe6c
leedongha1998 <leedongha98@gmail.com>
2025-04-08 12:05:05 +0900

    [#32][thread] refactor : thread page response 수정

--- thread/src/main/java/com/newpick4u/thread/thread/presentation/controller/ThreadApiController.java
+++ thread/src/main/java/com/newpick4u/thread/thread/presentation/controller/ThreadApiController.java
@@ -8,14 +8,14 @@ import com.newpick4u.thread.thread.application.usecase.ThreadService;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,12 +26,16 @@ public class ThreadApiController {
   private final ThreadService threadService;
 
   @GetMapping()
-  public ResponseEntity<PageResponse<ThreadResponseDto>> getThreads(
-      @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+  public ResponseEntity<ApiResponse<PageResponse<ThreadResponseDto>>> getThreads(
+      @RequestParam(defaultValue = "0") int page,
+      @RequestParam(defaultValue = "10") int size
+  ) {
 
+    Pageable pageable = PageRequest.of(page, size);
     Page<ThreadResponseDto> response = threadService.getThreads(pageable);
-    return ResponseEntity.ok(PageResponse.from(response));
 
+    return ResponseEntity.status(HttpStatus.OK.value())
+        .body(ApiResponse.of(HttpStatus.OK, PageResponse.from(response)));
   }
 
   @GetMapping("/{threadId}")
