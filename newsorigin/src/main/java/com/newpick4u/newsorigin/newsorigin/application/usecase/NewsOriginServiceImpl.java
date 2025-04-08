1db7bdef77f5caff89a28949ed7e63c73eba8605
jeonjinwoo <wls45al@gmail.com>
2025-04-08 18:50:10 +0900

    [#47][newsorigin] feat: 원본뉴스 전송기능 방어 로직 추가

--- newsorigin/src/main/java/com/newpick4u/newsorigin/newsorigin/application/usecase/NewsOriginServiceImpl.java
+++ newsorigin/src/main/java/com/newpick4u/newsorigin/newsorigin/application/usecase/NewsOriginServiceImpl.java
@@ -11,6 +11,7 @@ import com.newpick4u.newsorigin.newsorigin.application.parser.BodyParser;
 import com.newpick4u.newsorigin.newsorigin.domain.entity.NewsOrigin;
 import com.newpick4u.newsorigin.newsorigin.domain.repository.NewsOriginRepository;
 import io.micrometer.common.util.StringUtils;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -26,6 +27,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class NewsOriginServiceImpl implements NewsOriginService {
 
+  private static final int SEND_BATCH_SIZE = 20;
   private final OriginCollectClient originCollectClient;
   private final GetOriginBodyClient getOriginBodyClient;
   private final NewsOriginRepository newsOriginRepository;
@@ -60,12 +62,47 @@ public class NewsOriginServiceImpl implements NewsOriginService {
 
     // RDB 조회
     List<NewsOrigin> beforeSentNewsOrigin = newsOriginRepository.getAllByBeforeSentQueue();
-
-    ExecutorService threadPool = Executors.newFixedThreadPool(beforeSentNewsOrigin.size());
-    ArrayList<CompletableFuture<Void>> sendTaskList = new ArrayList<>();
-
     AtomicInteger updateCount = new AtomicInteger(0);
+    if (beforeSentNewsOrigin.isEmpty()) {
+      return updateCount.get();
+    }
 
+    ArrayDeque<NewsOrigin> beforeSentNewsOriginQueue = new ArrayDeque<>(beforeSentNewsOrigin);
+    ExecutorService threadPool = Executors.newFixedThreadPool(SEND_BATCH_SIZE);
+
+    // BATCH_SIZE 만큼 나눠서 작업 수행
+    while (!beforeSentNewsOriginQueue.isEmpty()) {
+      ArrayList<NewsOrigin> currentTarget = new ArrayList<>();
+      while (!beforeSentNewsOriginQueue.isEmpty() && currentTarget.size() < SEND_BATCH_SIZE) {
+        currentTarget.add(beforeSentNewsOriginQueue.pop());
+      }
+      sendTask(threadPool, currentTarget, updateCount);
+
+      try {
+        Thread.sleep(3000);
+      } catch (InterruptedException e) {
+        log.error("InterruptedException : sendNewsOriginMessages.sleep ", e);
+      }
+    }
+
+    threadPool.shutdown();
+    log.info("threadPool isShutdown : {}", threadPool.isShutdown());
+
+    return updateCount.get();
+  }
+
+  /**
+   * 지정된 List 만큼 원본획득 ~ 메세지 전송 작업을 수행: NewsOrigin 하나씩 원본작업획득, 메세지전송, 상태업데이트 수행
+   *
+   * @param threadPool           동시작업을 수행할 쓰레드풀
+   * @param beforeSentNewsOrigin 전송 작업 전 NewsOrigin 목록
+   * @param updateCount          업데이트 된 수를 카운트 하기 위한 변수
+   */
+  private void sendTask(
+      ExecutorService threadPool,
+      List<NewsOrigin> beforeSentNewsOrigin,
+      AtomicInteger updateCount) {
+    ArrayList<CompletableFuture<Void>> sendTaskList = new ArrayList<>();
     for (NewsOrigin newsOrigin : beforeSentNewsOrigin) {
       CompletableFuture<Void> sendTask = CompletableFuture.runAsync(() -> {
 
@@ -105,6 +142,5 @@ public class NewsOriginServiceImpl implements NewsOriginService {
     }
 
     sendTaskList.forEach(CompletableFuture::join);
-    return updateCount.get();
   }
 }
