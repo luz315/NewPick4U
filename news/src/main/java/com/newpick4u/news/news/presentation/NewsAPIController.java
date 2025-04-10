eedef09803103b96a0fd806d6f779fefc7c83646
poorm <315luz10@gmail.com>
2025-04-10 22:47:23 +0900

    [#52][news] refactor: api 수정

--- news/src/main/java/com/newpick4u/news/news/presentation/NewsAPIController.java
+++ news/src/main/java/com/newpick4u/news/news/presentation/NewsAPIController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("api/v1/news")
+@RequestMapping("/api/v1/news")
 @RequiredArgsConstructor
 public class NewsAPIController {
     private final NewsService newsService;
