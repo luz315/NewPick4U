6d72f9b54d71d712d5321909ab95c5fc5c960ae4
venus01 <geumsung7769@naver.com>
2025-04-21 00:39:52 +0900

    [#94][advertisement] feat : 부하테스트 관련 설정 추가
    - kafka 메시지 처리 및 응용계층의 성능테스트를 위한 설정을 추가하였음

--- client/src/main/java/com/newpick4u/client/advertisement/infrastructure/kafka/PointRequestConsumer.java
+++ client/src/main/java/com/newpick4u/client/advertisement/infrastructure/kafka/PointRequestConsumer.java
@@ -1,7 +1,7 @@
 package com.newpick4u.client.advertisement.infrastructure.kafka;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.newpick4u.client.advertisement.application.exception.AdvertisementException.NotFoundException;
 import com.newpick4u.client.advertisement.application.message.request.PointRequestMessage;
 import com.newpick4u.client.advertisement.application.usecase.AdvertisementService;
 import lombok.RequiredArgsConstructor;
@@ -19,21 +19,30 @@ public class PointRequestConsumer {
   private final ObjectMapper objectMapper;
   private final AdvertisementService advertisementService;
 
+  private void verifyNotFoundException(Exception exception, Acknowledgment acknowledgment)
+      throws Exception {
+    if (exception instanceof NotFoundException) {
+      acknowledgment.acknowledge(); // 존재하지 않는 광고의 경우 재시도 하지 못하게 함
+      return;
+    }
+    throw new RuntimeException();
+  }
+
   @KafkaListener(topics = "${spring.kafka.consumer.topics.point-request}",
       groupId = "${spring.kafka.consumer.groups.point-request}",
       containerFactory = "updateMessageConcurrentKafkaListenerContainerFactory"
   )
-  public void consume(ConsumerRecord<String, String> record, Acknowledgment acknowledgment)
-      throws JsonProcessingException {
+  public void consume(ConsumerRecord<String, String> record, Acknowledgment ack)
+      throws Exception {
     log.info("Kafka Raw Value: {}", record.value());
     try {
       PointRequestMessage message = objectMapper.readValue(record.value(),
           PointRequestMessage.class);
       advertisementService.updatePointGrantedCount(message);
-      acknowledgment.acknowledge();
+      ack.acknowledge();
     } catch (Exception e) {
       log.error("Kafka consume error", e);
-      throw new RuntimeException();
+      verifyNotFoundException(e, ack);
     }
   }
 }
