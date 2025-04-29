67fff0b5f65685ae849106793de6339aeabbf4b9
venus01 <geumsung7769@naver.com>
2025-04-30 03:29:43 +0900

    [#117][advertisement] fix : NewsClient 수정
    - 요청을 보낼 때 CurrentUserInfoDto를 같이 보내는 과정에서 발생하는 오류로 인해 요청양식을 수정하였음

--- client/src/main/java/com/newpick4u/client/advertisement/infrastructure/client/NewsClientImpl.java
+++ client/src/main/java/com/newpick4u/client/advertisement/infrastructure/client/NewsClientImpl.java
@@ -3,7 +3,6 @@ package com.newpick4u.client.advertisement.infrastructure.client;
 import com.newpick4u.client.advertisement.application.client.NewsClient;
 import com.newpick4u.client.advertisement.application.dto.response.GetNewsResponseDto;
 import com.newpick4u.client.global.config.FeignConfig;
-import com.newpick4u.common.resolver.dto.CurrentUserInfoDto;
 import com.newpick4u.common.response.ApiResponse;
 import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -15,6 +14,5 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface NewsClientImpl extends NewsClient {
 
   @GetMapping("internal/v1/news/{newsId}")
-  ResponseEntity<ApiResponse<GetNewsResponseDto>> getNews(@PathVariable("newsId") UUID newsId,
-      CurrentUserInfoDto userInfo);
+  ResponseEntity<ApiResponse<GetNewsResponseDto>> getNews(@PathVariable("newsId") UUID newsId);
 }
