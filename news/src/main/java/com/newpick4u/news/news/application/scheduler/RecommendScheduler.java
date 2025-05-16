554f0b34b3c58fbad40bbcf1b382f6379c937130
poorm <315luz10@gmail.com>
2025-05-17 02:13:28 +0900

    [#84][news] refactor: 뉴스 추천 스케줄러 병렬처리 수정

--- news/src/main/java/com/newpick4u/news/news/application/scheduler/RecommendScheduler.java
+++ news/src/main/java/com/newpick4u/news/news/application/scheduler/RecommendScheduler.java
@@ -12,8 +12,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @Slf4j
 @Component
@@ -24,7 +24,7 @@ public class RecommendScheduler {
     private final RecommendationCacheOperator recommendationCacheOperator;
     private final VectorConverter vectorConverterProvider;
     private final NewsRecommender newsRecommender;
-    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
+    private final ExecutorService recommendExecutor;
 
     @Scheduled(cron = "0 0 3 * * *")
     public void updateAllUserRecommendations() {
@@ -33,21 +33,37 @@ public class RecommendScheduler {
         List<Long> userIds = new ArrayList<>(recommendationCacheOperator.getCachedUserIds());
         List<UUID> candidateIds = newsRepository.findAllActiveNewsIds();
 
-        for (Long userId : userIds) {
-            executorService.submit(() -> updateSingleUserRecommendation(userId, candidateIds));
-        }
+        List<CompletableFuture<Boolean>> tasks = userIds.stream()
+                .map(userId -> CompletableFuture
+                        .supplyAsync(() -> updateSingleUserRecommendation(userId, candidateIds), recommendExecutor)
+                        .exceptionally(ex -> {
+                            log.error("[추천 실패] userId={}, 에러={}", userId, ex.getMessage(), ex);
+                            return false;
+                        })
+                )
+                .toList();
+
+        // 모든 작업 완료 대기
+        CompletableFuture.allOf(tasks.toArray(new CompletableFuture[0])).join();
+
+        // 성공 개수 집계
+        long successCount = tasks.stream()
+                .map(CompletableFuture::join)
+                .filter(Boolean::booleanValue)
+                .count();
+
+        log.info("[배치 완료] 총 사용자 수: {}, 성공 수: {}", userIds.size(), successCount);
     }
 
-    private void updateSingleUserRecommendation(Long userId, List<UUID> candidateIds) {
+    private boolean updateSingleUserRecommendation(Long userId, List<UUID> candidateIds) {
         try {
             // 사용자 태그 로그 점수표 가져오기
             Map<String, Double> userTagMap = recommendationCacheOperator.getUserTagScore(userId);
-            if (userTagMap.isEmpty()) return;
-
+            if (userTagMap.isEmpty()) return false;
 
             // 전역 태그 인덱스는 Redis에서 가져오기
             List<String> tagIndexList = recommendationCacheOperator.getGlobalTagIndexList();
-            if (tagIndexList.isEmpty()) return;
+            if (tagIndexList.isEmpty()) return false;
 
             // 1. 벡터화 (only 전역태그 인덱스, 사용자 태그로그 / 뉴스 벡터화는 스케줄러가 진행)
             double[] userVector = vectorConverterProvider.toUserVector(userTagMap, tagIndexList);
@@ -55,18 +71,18 @@ public class RecommendScheduler {
             // 2. 유사도 계산
             List<UUID> recommended = newsRecommender.recommendByContentVector(userVector, candidateIds);
 
-            // 3. Redis에 캐싱
-            List<String> recommendedIds = recommended.stream()
-                    .map(UUID::toString) // newsId 자체일 경우
-                    .toList();
-
             if (recommended.isEmpty()) {
                 log.info("[추천 없음] userId={}", userId);
-                return;
+                return true;
             }
 
+            // 3. Redis에 캐싱
+            List<String> recommendedIds = recommended.stream()
+                    .map(UUID::toString)
+                    .toList();
             recommendationCacheOperator.storeRecommendedNews(userId, recommendedIds);
             log.info("[추천 저장 완료] userId={}, count={}", userId, recommendedIds.size());
+            return true;
         } catch (Exception e) {
             log.error("[추천 저장 실패] userId={}", userId, e);
             throw CustomException.from(NewsErrorCode.NEWS_RECOMMENDATION_FAIL);
